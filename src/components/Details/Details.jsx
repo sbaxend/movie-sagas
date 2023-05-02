52771ac7285a08b6832969f495d0ed97fa041caa
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-05-02 11:31:05 -0400

    added more Material UI and tried out new MUI

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -4,16 +4,20 @@ import {
   useParams,
 } from "react-router-dom/cjs/react-router-dom.min";
 
+import Typography from "@mui/material/Typography";
 import React, { useEffect } from "react";
-
+import Avatar from '@mui/material/Avatar';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
 
 function Details() {
   const selectedMovie = useSelector((store) => store.selectedMovie);
   const dispatch = useDispatch();
   const history = useHistory();
+  //once fetch genre is done it will set the genres in store
+  //the genres of the selected movie will display
   const genres = useSelector((store) => store.genres);
 
   const backHome = () => {
@@ -21,7 +25,7 @@ function Details() {
     dispatch({ type: "CLEAR_GENRE" });
     history.push("/");
   };
-
+  // this fetches sends to the the store which triggers the generator function* fetchGenres
   useEffect(() => {
     dispatch({ type: "FETCH_GENRES", payload: selectedMovie.id });
   }, []);
@@ -30,32 +34,27 @@ function Details() {
   //
   return (
     <div>
-     <Paper elevation={3} style={{ padding: '1rem' }}>
-    <header>
-    <h1>{selectedMovie.title}</h1>
-    <img src={selectedMovie.poster} />
-  </header>
-
-  <section>
-   
-    <h2>Description</h2>
-    <p>{selectedMovie.description}</p>
-    
-
-    <h2>Genre</h2>
-    <ul>
-      {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
-      ))}
-    </ul>
-  </section>
-
-  <footer>
-    <Button onClick={backHome}><ArrowBackIcon /></Button>
-  </footer>
-  </Paper>
+    <Paper elevation={5} style={{ padding: '1rem' }}>
+      <Avatar src={selectedMovie.poster} style={{ width: '10rem', height: '10rem', marginBottom: '1rem' }} />
+      <Typography variant="h4" >{selectedMovie.title}</Typography>
+      <Typography variant="body1">{selectedMovie.description}</Typography>
+     
+      <div style={{ display: 'flex', flexWrap: 'wrap', marginBottom: '1rem' }}>
+        {genres.map((genre) => (
+          <Chip key={genre.id} label={genre.name} style={{ margin: '0.25rem' }} />
+        ))}
+      </div>
+      <Button onClick={backHome}><ArrowBackIcon /></Button>
+    </Paper>
   </div>
-  );
+);
 }
+
+
+
+
+
+
+
 
 export default Details;
